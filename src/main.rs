abeb76823c8e51bb388da69521e7f1b3e11e63f9
Niklas Halonen <niklas.halonen@eficode.com>
2022-03-03 15:42:36 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{self, Body, Request, Response, Server};
+use std::net::SocketAddr;
+
+async fn tunkki(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let whole_body = hyper::body::to_bytes(req.into_body()).await?;
+
+    println!("{}", std::str::from_utf8(&whole_body).unwrap());
+
+    Ok(Response::new(Body::empty()))
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 9099));
+
+    let make_service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(tunkki)) });
+
+    Server::bind(&addr).serve(make_service).await.unwrap();
+}
